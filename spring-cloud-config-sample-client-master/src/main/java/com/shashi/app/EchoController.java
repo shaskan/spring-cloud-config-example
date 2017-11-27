356bdf8020df76a06eedd7ef0373145879cd049c
Shashi Kant <shashi.kant@accenture.com>
2017-11-27 18:02:48 +0530

    First commit

---
+++ spring-cloud-config-sample-client-master/src/main/java/com/shashi/app/EchoController.java
@@ -0,0 +1,19 @@
+package com.shashi.app;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope
+public class EchoController {
+
+    @Value("${provider-name}")
+    private String name = "nowhere";
+
+    @RequestMapping("/")
+    public String home() {
+        return "This is a dynamic property loaded from " + name;
+    }
+}
